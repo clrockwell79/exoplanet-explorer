f74d7cf87c540982c4eb02dc6cdfe745e3e0f887
Chris Rockwell <chris@chrisrockwell.com>
2019-03-02 20:21:57 -0500

    lesson 3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -42,7 +42,7 @@ Instructions:
    */
   function get(url) {
     return fetch(url, {
-      method: 'get'
+      method: 'get',
     });
   }
 
@@ -64,6 +64,13 @@ Instructions:
 
     Your code goes here!
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+      .then(function(response) {
+        addSearchHeader(response.query);
+        return getJSON(response.results[0]);
+      })
+      .then(function(data) {
+        createPlanetThumb(data);
+      });
   });
 })(document);
