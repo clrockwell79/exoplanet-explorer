5df85ffa71989b2f203ecd5432becbb881ee2003
Chris Rockwell <chris@chrisrockwell.com>
2019-03-02 19:09:33 -0500

    lesson 1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@ Instructions:
    */
   function addSearchHeader(response) {
     try {
-      response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
+      response = JSON.parse(response).query; // you'll be moving this line out of here in the next quiz!
     } catch (e) {
       // it's 'unknown', so leave it alone
     }
@@ -36,22 +36,21 @@ Instructions:
     /*
     This code needs to get wrapped in a Promise!
      */
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
-        // It failed :(
-        // Be nice and reject with req.statusText
-      }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+      req.onload = function() {
+        if (req.status === 200) {
+          resolve(req.response);
+        } else {
+          reject(Error(req.statusText));
+        }
+      };
+      req.onerror = function() {
+        reject(Error('Network Error'));
+      };
+      req.send();
+    });
   }
 
   window.addEventListener('WebComponentsReady', function() {
@@ -61,6 +60,11 @@ Instructions:
     You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
     pass 'unknown' to addSearchHeader if it rejects.
      */
-    // get('../data/earth-like-results.json')
+    get('../data/earth-like-results.json')
+      .then(addSearchHeader)
+      .catch(function(val) {
+        console.log(val);
+        addSearchHeader('unknown');
+      });
   });
 })(document);
